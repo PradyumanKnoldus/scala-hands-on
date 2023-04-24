550c3f780aa3c85ed17d50160b2128f168402814
PradyumanKnoldus <pradyuman.singh@knoldus.com>
2023-04-24 17:04:31 +0530

    Added Program of Higher Order Function returning function

--- src/main/scala/functions/HigherOrderFunction.scala
+++ src/main/scala/functions/HigherOrderFunction.scala
@@ -4,6 +4,8 @@ package functions
 import functions.AnonymousFunction.addNumbers
 
 object HigherOrderFunction extends App {
+
+  // HOF that takes a function as an argument
   def adder(firstNumber: Int, secondNumber: Int, function: (Int, Int) => Int): Int = {
     function(firstNumber, secondNumber)
   }
@@ -11,4 +13,16 @@ object HigherOrderFunction extends App {
   val result = adder(10,20, addNumbers)
 
   println(result)
+
+  // HOF that returns a function
+  def add(firstNumber: Int): Int => Int = {
+    (secondNumber: Int) => firstNumber + secondNumber
+  }
+
+  val addFive = add(5)
+  val addedNumber = addFive(5)
+
+  println(addedNumber)
+
+
 }
